812102384f37ab50b7bd0a0b65c1d3b95a7ee401
Thijs Schreijer <thijs@thijsschreijer.nl>
2025-01-08 22:24:57 +0100

    docs(examples): add examples

---
+++ examples/colors.lua
@@ -0,0 +1,54 @@
+-- This example demonstrates the use of the text-attribute stack, and how to
+-- use it to manage text attributes in a more structured way.
+
+local sys = require("system")
+local t = require("terminal")
+
+
+-- initialize terminal; backup (switch to alternate buffer) and set output to stdout
+t.initialize(true, io.stdout)
+
+-- clear the screen, and move cursor to top-left
+t.clear()
+t.cursor_push(1,1)
+
+-- push text attribues on the stack
+t.textpush{
+  fg = "white",
+  brightness = "dim",
+}
+t.print("Hello dim white World!")
+
+
+t.textpush{
+  fg = "white",
+  bg = "blue",
+  brightness = "normal",
+}
+t.print("Hello white on blue World!")
+
+
+t.textpush{
+  fg = "red",
+  bg = "black",
+  brightness = "bright",
+}
+t.print("Hello bright red World!")
+
+-- Unwind the stack, and restore text attributes along the way
+t.textpop()
+t.print("Hello white on blue World! (again)")
+
+t.textpop()
+t.print("Hello dim white World! (again)")
+
+t.textpop()
+t.write("Press any key, or wait 5 seconds...")
+t.flush()
+sys.readansi(5)
+
+-- restore all settings (reverts to original screen buffer)
+t.shutdown()
+
+-- this is printed on the original screen buffer
+print("done!")
---
+++ examples/testscreen.lua
@@ -0,0 +1,59 @@
+local sys = require("system")
+local t = require("terminal")
+
+
+-- writes entire screen with numbers 1-9
+local function testscreen()
+  local r, c = sys.termsize()
+  local row = ("1234567890"):rep(math.floor(c/10) + 1):sub(1, c)
+
+  -- push a color on the stack
+  t.textpush{
+    fg = "red",
+    brightness = "dim",
+  }
+
+  -- print all rows to fill the screen
+  for i = 1, r do
+    t.cursor_set(i, 1)
+    t.write(row)
+  end
+
+  -- pop the color previously set, restoring the previous setting
+  t.textpop()
+end
+
+
+
+
+-- initialize terminal; backup (switch to alternate buffer) and set output to stdout
+t.initialize(true, io.stdout)
+
+-- clear the screen, and draw the test screen
+t.clear()
+testscreen()
+
+-- draw a box, with 2 cols/rows margin around the screen
+local edge = 2
+local r,c = sys.termsize()
+t.cursor_set(edge+1, edge+1)
+t.box(r - 2*edge, c - 2*edge, t.box_fmt.double, true, "test screen")
+
+-- move cursor inside the box
+t.cursor_move(1, 1)
+
+-- set text attributes (not using the stack this time)
+t.textset{
+  fg = "red",
+  bg = "blue",
+  brightness = 3,
+}
+t.write("Hello World! press any key, or wait 5 seconds...")
+t.flush()
+sys.readansi(5)
+
+-- restore all settings (reverts to original screen buffer)
+t.shutdown()
+
+-- this is printed on the original screen buffer
+print("done!")
