7a723cab379bbcfc808c5f3edd859d5f6ab38964
Tin Nguyen (WSL Ubuntu) <nguyenvb.tin03@gmail.com>
2025-03-25 01:41:32 -0400

    style(example): mass rename to fit lua snake_case

--- examples/headers.lua
+++ examples/headers.lua
@@ -1,5 +1,8 @@
 local sys = require("system")
 local t = require("terminal")
+local to = t.output
+local tts = t.text.stack
+local tcp = t.cursor.position
 
 -- Keys
 local key_names = {
@@ -22,21 +25,28 @@ local key_names = {
   ["\2"] = "ctrl-b",
 }
 -- Utility function to read a key from terminal input
-local function readKey()
+local function read_key()
   local key = t.input.readansi(1) -- Read a single key
   return key, key_names[key] or key -- Return raw key and mapped name
 end
 
 -- Utility function to draw components using the stack style
-local function withStyle(style, callback)
-  t.text.stack.push(style)
+local function with_style(style, callback)
+  tts.push(style)
   callback()
-  t.text.stack.pop()
+  tts.pop()
 end
 
 -- Colors
 local colors = {
-  "black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"
+  "black",
+  "red",
+  "green",
+  "yellow",
+  "blue",
+  "magenta",
+  "cyan",
+  "white",
 }
 local rcolors = {}
 for index, color in ipairs(colors) do
@@ -52,7 +62,7 @@ local TextArea = {}
 function TerminalUIApp:new(options)
   options = options or {}
   local instance = {
-    appName = options.appName or "Terminal Application",
+    app_name = options.app_name or "Terminal Application",
     header = options.header or Bar:new(),
     footer = options.footer or Bar:new(),
     content = options.content or TextArea:new(),
@@ -67,27 +77,27 @@ function TerminalUIApp:new(options)
 end
 
 function TerminalUIApp:run()
-  t.initialize {
+  t.initialize({
     displaybackup = true,
     filehandle = io.stdout,
-  }
+  })
   t.clear.screen()
 
-  self.content:initializeContent()
-  self.content:handleInput()
+  self.content:draw()
+  self.content:handle_input()
 
   t.shutdown()
-  print("Thank you for using MyTerminal! You wrote " .. self.linesWritten .. " lines.")
+  print("Thank you for using my_terminal! You wrote " .. self.linesWritten .. " lines.")
 end
 
-function TerminalUIApp:refreshDisplay()
+function TerminalUIApp:refresh_display()
   local savedY, savedX = self.content.cursorY, self.content.cursorX
   local rows, _ = sys.termsize()
 
   self.header:draw(1)
   self.footer:draw(rows)
 
-  self.content:updateCursor(savedY, savedX)
+  self.content:update_cursor(savedY, savedX)
 end
 
 -- Constructor: TextArea: main content area
@@ -99,104 +109,104 @@ function TextArea:new(options)
     cursorX = 2,
     style = options.style or { fg = "green", bg = "black", brightness = "normal" },
   }
-  instance.currentFgColorIndex = instance.style.fg and rcolors[instance.style.fg] or 3
-  instance.currentBgColorIndex = instance.style.bg and rcolors[instance.style.bg] or 1
+  instance.index_fg = instance.style.fg and rcolors[instance.style.fg] or 3
+  instance.index_bg = instance.style.bg and rcolors[instance.style.bg] or 1
   setmetatable(instance, { __index = self })
   return instance
 end
 
 -- cycle colors
-function TextArea:cycleColor(isBackground)
+function TextArea:cycle_color(isBackground)
   if isBackground then
-    self.currentBgColorIndex = (self.currentBgColorIndex % #colors) + 1
-    self.style.bg = colors[self.currentBgColorIndex]
+    self.index_bg = (self.index_bg % #colors) + 1
+    self.style.bg = colors[self.index_bg]
   else
-    self.currentFgColorIndex = (self.currentFgColorIndex % #colors) + 1
-    self.style.fg = colors[self.currentFgColorIndex]
+    self.index_fg = (self.index_fg % #colors) + 1
+    self.style.fg = colors[self.index_fg]
   end
 
-  self:initializeContent()
-  self.parent_app:refreshDisplay()
+  self:draw()
+  self.parent_app:refresh_display()
 end
 
-function TextArea:getCurrentColorInfo()
-  return string.format("FG: %s, BG: %s", colors[self.currentFgColorIndex], colors[self.currentBgColorIndex])
+function TextArea:color_info()
+  return string.format("FG: %s, BG: %s", colors[self.index_fg], colors[self.index_bg])
 end
 
-function TextArea:updateCursor(y, x)
+function TextArea:update_cursor(y, x)
   self.cursorY = y
   self.cursorX = x
-  t.cursor.position.set(y, x)
+  tcp.set(y, x)
 end
 
-function TextArea:initializeContent()
+function TextArea:draw()
   local rows, cols = sys.termsize()
 
-  t.text.stack.pop()
-  t.text.stack.push(self.style)
+  tts.pop()
+  tts.push(self.style)
 
   for i = 2, rows - 1 do
-    t.cursor.position.set(i, 1)
-    t.output.write(string.rep(" ", cols))
+    tcp.set(i, 1)
+    to.write(string.rep(" ", cols))
   end
 
-  self:updateCursor(2, 2)
+  self:update_cursor(2, 2)
 end
 
-function TextArea:handleInput()
+function TextArea:handle_input()
   local rows, cols = sys.termsize()
 
-  self.parent_app:refreshDisplay()
+  self.parent_app:refresh_display()
 
   while true do
-    t.cursor.position.set(self.cursorY, self.cursorX)
+    tcp.set(self.cursorY, self.cursorX)
 
-    local rawKey, keyName = readKey()
+    local key_raw, key_name = read_key()
 
-    if rawKey then
-      if keyName == "escape" or keyName == "f10" then
+    if key_raw then
+      if key_name == "escape" or key_name == "f10" then
         break
-      elseif keyName == "ctrl-f" then
-        self:cycleColor(false)
-      elseif keyName == "ctrl-b" then
-        self:cycleColor(true)
-      elseif keyName == "enter" then
+      elseif key_name == "ctrl-f" then
+        self:cycle_color(false)
+      elseif key_name == "ctrl-b" then
+        self:cycle_color(true)
+      elseif key_name == "enter" then
         self.parent_app.linesWritten = self.parent_app.linesWritten + 1
 
         if self.cursorY < rows - 1 then
-          self:updateCursor(self.cursorY + 1, 2)
+          self:update_cursor(self.cursorY + 1, 2)
         else
-          self:updateCursor(self.cursorY, 2)
-          t.output.write(string.rep(" ", cols))
+          self:update_cursor(self.cursorY, 2)
+          to.write(string.rep(" ", cols))
         end
-        self.parent_app:refreshDisplay()
-      elseif keyName == "backspace" then
+        self.parent_app:refresh_display()
+      elseif key_name == "backspace" then
         if self.cursorX > 2 then
-          self:updateCursor(self.cursorY, self.cursorX - 1)
-          t.output.write(" ")
-          self:updateCursor(self.cursorY, self.cursorX)
+          self:update_cursor(self.cursorY, self.cursorX - 1)
+          to.write(" ")
+          self:update_cursor(self.cursorY, self.cursorX)
         elseif self.cursorY > 2 then
-          self:updateCursor(self.cursorY - 1, cols - 2)
+          self:update_cursor(self.cursorY - 1, cols - 2)
         end
-      elseif keyName == "up" and self.cursorY > 2 then
-        self:updateCursor(self.cursorY - 1, self.cursorX)
-      elseif keyName == "down" and self.cursorY < rows - 1 then
-        self:updateCursor(self.cursorY + 1, self.cursorX)
-      elseif keyName == "right" and self.cursorX < cols then
-        self:updateCursor(self.cursorY, self.cursorX + 1)
-      elseif keyName == "left" and self.cursorX > 2 then
-        self:updateCursor(self.cursorY, self.cursorX - 1)
-      elseif keyName == "home" then
-        self:updateCursor(self.cursorY, 2)
-      elseif keyName == "end" then
-        self:updateCursor(self.cursorY, cols - 1)
-      elseif #rawKey == 1 then
-        t.output.write(rawKey)
-        self:updateCursor(self.cursorY, self.cursorX + 1)
+      elseif key_name == "up" and self.cursorY > 2 then
+        self:update_cursor(self.cursorY - 1, self.cursorX)
+      elseif key_name == "down" and self.cursorY < rows - 1 then
+        self:update_cursor(self.cursorY + 1, self.cursorX)
+      elseif key_name == "right" and self.cursorX < cols then
+        self:update_cursor(self.cursorY, self.cursorX + 1)
+      elseif key_name == "left" and self.cursorX > 2 then
+        self:update_cursor(self.cursorY, self.cursorX - 1)
+      elseif key_name == "home" then
+        self:update_cursor(self.cursorY, 2)
+      elseif key_name == "end" then
+        self:update_cursor(self.cursorY, cols - 1)
+      elseif #key_raw == 1 then
+        to.write(key_raw)
+        self:update_cursor(self.cursorY, self.cursorX + 1)
       end
     end
 
-    t.output.flush()
+    to.flush()
   end
 end
 
@@ -214,9 +224,9 @@ end
 function Bar:draw(row)
   local _, cols = sys.termsize()
 
-  withStyle(self.style, function()
-    t.cursor.position.set(row, 1)
-    t.output.write(string.rep(" ", cols))
+  with_style(self.style, function()
+    tcp.set(row, 1)
+    to.write(string.rep(" ", cols))
 
     if self.content_fn then
       self:content_fn(row, cols)
@@ -225,46 +235,46 @@ function Bar:draw(row)
 end
 
 -- Initiate two bars of Bar for header and footer
-local myHeader = Bar:new {
+local my_header = Bar:new({
   style = { fg = "white", bg = "blue", brightness = "bright" },
   content_fn = function(self, _, cols)
     local currentTime = os.date("%H:%M:%S")
     local cursorText = string.format("Pos: %d,%d", self.parent_app.content.cursorY, self.parent_app.content.cursorX)
-    t.cursor.position.set(1, 2)
-    t.output.write(self.parent_app.appName)
+    tcp.set(1, 2)
+    to.write(self.parent_app.app_name)
 
     local clockPos = math.floor(cols / 4)
-    t.cursor.position.set(1, clockPos)
-    t.output.write(currentTime)
+    tcp.set(1, clockPos)
+    to.write(currentTime)
 
     local cursorPos = math.floor(cols / 2) + 5
-    t.cursor.position.set(1, cursorPos)
-    t.output.write(cursorText)
+    tcp.set(1, cursorPos)
+    to.write(cursorText)
 
-    local colorText = "Color: " .. self.parent_app.content:getCurrentColorInfo()
-    t.cursor.position.set(1, cols - #colorText - 1)
-    t.output.write(colorText)
+    local colorText = "Color: " .. self.parent_app.content:color_info()
+    tcp.set(1, cols - #colorText - 1)
+    to.write(colorText)
   end,
-}
-local myFooter = Bar:new {
+})
+local my_footer = Bar:new({
   style = { fg = "white", bg = "blue", brightness = "bright" },
   content_fn = function(self, _, cols)
     local rows, _ = sys.termsize()
     local lineText = "Lines: " .. self.parent_app.linesWritten
     local helpText = "Ctrl+F: Change FG | Ctrl+B: Change BG | ESC: Exit"
 
-    t.cursor.position.set(rows, 2)
-    t.output.write(lineText)
+    tcp.set(rows, 2)
+    to.write(lineText)
 
-    t.cursor.position.set(rows, cols - #helpText - 1)
-    t.output.write(helpText)
+    tcp.set(rows, cols - #helpText - 1)
+    to.write(helpText)
   end,
-}
+})
 
-local myTerminal = TerminalUIApp:new {
-  appName = "The best terminal ever",
-  header = myHeader,
-  footer = myFooter,
-}
+local my_terminal = TerminalUIApp:new({
+  app_name = "The best terminal ever",
+  header = my_header,
+  footer = my_footer,
+})
 
-myTerminal:run()
+my_terminal:run()
