d3b46b3fba1e55d9b574764234a69771cf407ea5
Thijs Schreijer <thijs@thijsschreijer.nl>
2025-04-09 23:03:22 +0200

    chore(docs): rerender docs

--- src/terminal/init.lua
+++ src/terminal/init.lua
@@ -208,8 +208,8 @@ end
 -- When an error occurs, and the application exits, the terminal might not be properly shut down.
 -- This function wraps a function in calls to `initialize` and `shutdown`, ensuring the terminal is properly shut down.
 -- If an error is caught, it first shutsdown the terminal and then rethrows the error.
--- @tparam[opt] table opts options table, to pass to `initialize`.
 -- @tparam function main the function to wrap
+-- @tparam[opt] table opts options table, to pass to `initialize`.
 -- @treturn function wrapped function
 -- @usage
 -- local function main(param1, param2)
