d6bbff6abc3ea688fe1ca91a0ca035c2ec7fe392
Tin Nguyen <nguyenvb.tin03@gmail.com>
2025-06-12 11:09:55 -0400

    fix(cli): cli module's ldoc description

--- src/terminal/cli/init.lua
+++ src/terminal/cli/init.lua
@@ -1,5 +1,6 @@
 --- CLI component module.
--- Provides utilities to handle the cursor in terminals.
+-- Provide CLI widgets for terminal applications
+-- NOTE: you MUST `terminal.initialize()` before calling these widgets' `:run()`
 -- @module terminal.cli
 local M = {}
 package.loaded["terminal.cli"] = M -- Register the module early to avoid circular dependencies
